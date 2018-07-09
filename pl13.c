548f24b2f7f93acd11ff4b63fed0bbc69b4cf24f
muthusamymukesh <35652643+muthusamymukesh@users.noreply.github.com>
2018-07-09 16:24:14 +0530

    Update pl13.c

--- pl13.c
+++ pl13.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
+#include<math.h>
 void main() {
-   int a,b,c,d,e,f ;
+   int a,b,c,i,d,e,f=0 ;
+  printf("Enter the size of value :");
+  scanf("%d",&e);
   printf("Enter the value :");
   scanf("%d",&a);
-  if(a!=0)
+  for(i=0;i<e;i++)
   {
       b=a%10;
+        f=f+pow(b,2);
       c=a/10;
-      d=c%10;
-      e=c/10;
-      f=(b^2)+(d^2)+(e^2);
-      printf("The sum of square of digites :%d",f);
   }
+      printf("%d",f);
 }
